7aae64fbef02d637697dd81eeae072949b8444e2
B-Sheley <112135501+B-Sheley@users.noreply.github.com>
2022-11-29 21:05:45 -0500

    update

---
+++ Time for Dinner/Enemy/Enemy_flying.gd
@@ -0,0 +1,35 @@
+extends KinematicBody2D
+
+onready var ray = $RayCast2D
+export var speed = 115
+export var look_speed = 30
+export var dmg = 20
+var health = 15
+var defense = 5
+var score = 10
+
+func _physics_process(_delta):
+	if Global.player == null:
+		Global.player = get_node_or_null("/root/Game/Player_Container/Player")
+	if Global.player != null:
+		ray.cast_to = ray.to_local(Global.player.global_position)
+		var c = ray.get_collider()
+		if c:
+			var velocity = ray.cast_to.normalized()*look_speed
+			if c.name == "Player":
+				velocity = ray.cast_to.normalized()*speed
+			move_and_slide(velocity, Vector2(0,0))
+		Global.player = null
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Player":
+		body.do_damage(dmg)
+
+func hp(d):
+	d -= defense
+	if d <= 0:
+		d = 1
+	health -= d
+	if health <= 0:
+		queue_free()
