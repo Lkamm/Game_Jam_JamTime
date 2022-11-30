b1c702c53e2c7e6f065ca45c988b718db1b909de
Lkamm <masterchiefmain@gmail.com>
2022-11-30 16:36:55 -0500

    done

--- Time for Dinner/Weapons/MeatBall.gd
+++ Time for Dinner/Weapons/MeatBall.gd
@@ -1,16 +1,33 @@
 extends KinematicBody2D
 
+var velocity = Vector2.ZERO
+var speed = 500.0
+var damage = 10
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var Explosion = load("res://Effects/Explosion.tscn")
+var Effects = null
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	velocity = Vector2(0,-speed).rotated(rotation)
+
+func _physics_process(_delta):
+	velocity = move_and_slide(velocity, Vector2.ZERO)
+	position.x = wrapf(position.x, 0, Global.VP.x)
+	position.y = wrapf(position.y, 0, Global.VP.y)
+	
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Area2D_body_entered(body):
+	if body.name != "Enemy":
+		if body.has_method("damage"):
+			body.damage(damage)
+		Effects = get_node_or_null("/root/Game/Effects")
+		#if Effects != null:
+		#	var explosion = Explosion.instance()
+		#	Effects.add_child(explosion)
+		#	explosion.global_position = global_position
+		queue_free()
+
+
+func _on_Timer_timeout():
+	queue_free()
