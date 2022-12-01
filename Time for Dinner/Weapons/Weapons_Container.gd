fac6850eefd72417f59c796adc1f4dbceb903297
Lkamm <masterchiefmain@gmail.com>
2022-12-01 18:07:49 -0500

    donezo

--- Time for Dinner/Weapons/Weapons_Container.gd
+++ Time for Dinner/Weapons/Weapons_Container.gd
@@ -1,10 +1,9 @@
 extends Node2D
 
 var cannon = load("res://Weapons/MeatCannon.tscn")
-var trap = load("res://Weapons/PastaTrap.tscn")
-var Player = get_node_or_null("/root/Game/Player_Container/Player")
+var trap = load("res://Weapons/Pasta_Trap.tscn")
 func _on_Cannon_mouse_entered():
-	$label.show()
+	$Cannon/Label.show()
 	if Input.is_action_pressed("attack") and "Sauce" in Global.Ingredients:
 		var cannons = cannon.instance()
 		cannons.global.position = Vector2(750, 300)
@@ -13,7 +12,7 @@ func _on_Cannon_mouse_entered():
 
 
 func _on_Trap_mouse_entered():
-	$Label.show()
+	$Pasta_Trap/Label.show()
 	if Input.is_action_just_pressed("attack") and "Pasta" in Global.Ingredients:
 		$Pasta_Trap/Pasta_Trap/Sprite.show()
 		$Pasta_Trap/Pasta_Trap/CollisionShape2D.disabled = false
@@ -21,7 +20,7 @@ func _on_Trap_mouse_entered():
 
 
 func _on_Trap2_mouse_entered():
-	$Label.show()
+	$Pasta_Trap2/Label.show()
 	if Input.is_action_just_pressed("attack") and "Pasta" in Global.Ingredients:
 		$Pasta_Trap2/Pasta_Trap/Sprite.show()
 		$Pasta_Trap2/Pasta_Trap/CollisionShape2D.disabled = false 
