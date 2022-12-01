2058c2030614ac8c836b14b0fae675c89f976aac
Lkamm <masterchiefmain@gmail.com>
2022-11-30 19:02:07 -0500

    done

--- Time for Dinner/Weapons/Weapons_Container.gd
+++ Time for Dinner/Weapons/Weapons_Container.gd
@@ -3,7 +3,7 @@ extends Node2D
 var cannon = load("res://Weapons/MeatCannon.tscn")
 var Player = get_node_or_null("/root/Game/Player_Container/Player")
 func _on_Cannon_mouse_entered():
-	if Input.is_action_pressed("click") and "Sauce" in Global.Ingredients:
+	if Input.is_action_pressed("interact") and "Sauce" in Global.Ingredients:
 		var cannons = cannon.instance()
 		cannons.global.position = Vector2(750, 300)
 		$Cannon.show()
