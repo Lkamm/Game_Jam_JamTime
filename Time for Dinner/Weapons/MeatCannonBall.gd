dda49242940a642d4de29d62b8f49e6ee72af12f
B-Sheley <112135501+B-Sheley@users.noreply.github.com>
2022-12-01 17:51:58 -0500

    Update

---
+++ Time for Dinner/Weapons/MeatCannonBall.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+var damage = 5
+var velocity = Vector2(10,0)
+
+onready var timer = $Timer
+
+
+
+func _ready():
+	pass
+
+func _physics_process(_delta):
+	position += velocity
+
+
+func _on_Cookie_body_entered(body):
+	if body.has_method("hp"):
+		body.hp(damage)
+		queue_free()
+
+func _on_Timer_timeout():
+	queue_free()
